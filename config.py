2637226095e26a4a657d06f3255be6656f32e6ff
abh628 <rockybhaiowner@gmail.com>
2025-08-13 22:44:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Vars For API End Pont.
-API_URL = getenv("API_URL", 'https://tgmusic.fallenapi.fun') #youtube song url
-API_KEY = getenv("API_KEY", "c0a054_fW2vswuUzMhw6isuc0xDqT53FovQF5AF") # youtube song api key, get it from https://t.me/MR_ROCKY_TZ
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", "30DxNexGenBots060d02") # youtube song api key, get it from https://t.me/MR_ROCKY_TZ
 
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", None)) 

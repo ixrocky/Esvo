53df005efca48788ced80b0d86ab9d842be853ee
ixrocky <shivamlkr629@gmail.com>
2025-11-06 16:19:47 +0530

    Update bot.py

--- SaregamaMusic/core/bot.py
+++ SaregamaMusic/core/bot.py
@@ -1,7 +1,3 @@
-import uvloop
-
-uvloop.install()
-
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
 
@@ -55,4 +51,3 @@ class AMBOTOP(Client):
 
     async def stop(self):
         await super().stop()
-
